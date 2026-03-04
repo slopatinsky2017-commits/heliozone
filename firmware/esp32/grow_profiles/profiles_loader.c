c58372feacffd6b3204eae03b3ca8e7510e00099
slopatinsky2017-commits <s.lopatinsky.2017@gmail.com>
2026-03-04 19:05:21 +0200

    Implement grow profiles structure and functions
    Added a structure to hold grow profiles and implemented initialization and printing functions.

--- firmware/esp32/grow_profiles/profiles_loader.c
+++ firmware/esp32/grow_profiles/profiles_loader.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char crop[32];
+    char stage[32];
+    int ppfd;
+    int photoperiod;
+    float dli;
+} grow_profile_t;
+
+grow_profile_t profiles[16];
+int profiles_count = 0;
 
 void grow_profiles_init()
 {
     printf("HelioZone: grow profiles module initialized\n");
+}
+
+void grow_profiles_print()
+{
+    for(int i = 0; i < profiles_count; i++)
+    {
+        printf("Crop: %s | Stage: %s | PPFD: %d | Photoperiod: %d | DLI: %.2f\n",
+               profiles[i].crop,
+               profiles[i].stage,
+               profiles[i].ppfd,
+               profiles[i].photoperiod,
+               profiles[i].dli);
+    }
 }
