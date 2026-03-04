50aa73a8bbd6a5fe99498053e05abd9615095a84
slopatinsky2017-commits <s.lopatinsky.2017@gmail.com>
2026-03-04 19:08:37 +0200

    Refactor grow engine to use grow_profile_t struct

--- firmware/esp32/components/grow_engine.c
+++ firmware/esp32/components/grow_engine.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+typedef struct {
+    char crop[32];
+    char stage[32];
+    int ppfd;
+    int photoperiod;
+    float dli;
+} grow_profile_t;
 
 float calculate_dli(int ppfd, int photoperiod_hours)
 {
@@ -8,12 +17,14 @@ float calculate_dli(int ppfd, int photoperiod_hours)
     return dli;
 }
 
-void grow_engine_print(int ppfd, int photoperiod)
+void grow_engine_run(grow_profile_t profile)
 {
-    float dli = calculate_dli(ppfd, photoperiod);
+    float dli = calculate_dli(profile.ppfd, profile.photoperiod);
 
     printf("HelioZone Grow Engine\n");
-    printf("PPFD: %d umol\n", ppfd);
-    printf("Photoperiod: %d h\n", photoperiod);
-    printf("DLI: %.2f mol/m2/day\n", dli);
+    printf("Crop: %s\n", profile.crop);
+    printf("Stage: %s\n", profile.stage);
+    printf("PPFD: %d umol\n", profile.ppfd);
+    printf("Photoperiod: %d h\n", profile.photoperiod);
+    printf("Target DLI: %.2f mol/m2/day\n", dli);
 }
